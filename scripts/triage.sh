6b162e86c47699526fc04cbed1aad245883c9250
ZawrzykrajLidia <lidia.zawrzykraj@gmail.com>
2026-05-28 17:53:02 +0200

    feat: add querying db for error details

--- scripts/triage.sh
+++ scripts/triage.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 LOG_FILE="../data/app.log"
+DB_FILE="../data/db.sqlite"
 
 echo "Analyzing logs for errors..."
 
 if grep -q "ERROR" "$LOG_FILE"; then
-	echo "Found errors in the logs! Processing..."
+	echo "[Alert] Found errors in the logs! Processing..."
 
 	grep "ERROR" "$LOG_FILE" | while read -r ERROR_LINE; do
 
@@ -13,6 +14,21 @@ if grep -q "ERROR" "$LOG_FILE"; then
 
 		if [ -n "$TXN_ID" ]; then
 			echo "-> Extracted Transaction ID: $TXN_ID"
+			echo "	 Querying the database for transaction details..."
+
+			DB_RESULT=$(sqlite3 "$DB_FILE" "SELECT customer_id, service_type, status FROM transactions WHERE transaction_id='$TXN_ID';")
+
+			if [ -n "$DB_RESULT" ]; then
+				CUST_ID=$(echo ${DB_RESULT} | cut -d "|" -f 1)
+				SERVICE=$(echo ${DB_RESULT} | cut -d "|" -f 2)
+				STATUS=$(echo ${DB_RESULT} | cut -d "|" -f 3)
+				echo "	 [!] TICKET DETAILS:"
+				echo "		Customer ID: $CUST_ID"
+				echo "		Service: $SERVICE"
+				echo "		DB Status: $STATUS"
+			else
+				echo "	 [!] WARNING: Transaction ID $TXN_ID not found in the databse!"
+			fi
 
 		else
 			echo "-> Found an ERROR line, but no Transaction ID attached."
